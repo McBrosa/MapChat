5bac1f46199100c29df2dd64a61f2941891eb708
ADBarb <banth94@vt.edu>
2016-04-13 19:09:57 -0400

    Chat = Dashboard now, footer update

--- src/java/com/mapchat/managers/LoginManager.java
+++ src/java/com/mapchat/managers/LoginManager.java
@@ -93,7 +93,7 @@ public class LoginManager implements Serializable {
       if (user.getUsername().equals(getUsername()) && user.getPassword().equals(getPassword())) {
         errorMessage = "";
         initializeSessionMap(user);
-        return "Chat";
+        return "Dashboard";
       }
       errorMessage = "Invalid username or password!";
       return "";
