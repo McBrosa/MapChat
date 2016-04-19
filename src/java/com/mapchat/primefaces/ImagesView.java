8bb1ab906daf0f953aa85d834317a569732a840d
ADBarb <banth94@vt.edu>
2016-04-19 14:21:38 -0400

    Minor header changes, create moved to its own XHTML, index now is a welcome page

---
+++ src/java/com/mapchat/primefaces/ImagesView.java
@@ -0,0 +1,28 @@
+/*
+ * Created by Anthony Barbee on 2016.04.19  * 
+ * Copyright © 2016 Anthony Barbee. All rights reserved. * 
+ */
+package com.mapchat.primefaces;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class ImagesView {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList();
+        for (int i = 1; i <= 12; i++) {
+            images.add("nature" + i + ".jpg");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
