2ce8290209064729b7011a83dccab232a95364e9
ACtheCoder <alan8@vt.edu>
2016-04-20 20:44:11 -0400

    js file for groups.xhtml, no functionality yet

---
+++ web/resources/js/groups.js
@@ -0,0 +1,17 @@
+/* 
+ * Created by Alan Cai on 2016.04.20  * 
+ * Copyright © 2016 Alan Cai. All rights reserved. * 
+ */
+
+function enterNewGroupName() {
+    
+}
+
+function enterUsernameToAdd() {
+    
+}
+function enterUsernameToRemove() {
+    
+}
+
+
