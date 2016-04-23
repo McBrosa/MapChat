fbfc57fe08aa6c164fbc3ab686ea7015b85eec5d
Nathan Rosa <rosaro77@live.com>
2016-04-23 13:53:01 -0400

    - Fixed updating current location bug

--- web/resources/js/map.js
+++ web/resources/js/map.js
@@ -22,21 +22,7 @@ function checkGeolocationByLoaderAPI() {
 }
 
 function setMapCenter(latitude, longitude) {
-//    PF('map').getMap().setCenter(new google.maps.LatLng(latitude, longitude));
-    document.getElementById('formId:latitude').value = latitude;
-    document.getElementById('formId:longitude').value = longitude;
-
-//    var icon = {
-//        //url: document.getElementById('formId:profilePic').value, // url
-//        scaledSize: new google.maps.Size(50, 50) // scaled size
-//                  origin: new google.maps.Point(0,0), // origin
-//                  anchor: new google.maps.Point(0, 0) // anchor
-//  };
-
-//    currentMarker = new google.maps.Marker({
-//        position:new google.maps.LatLng(latitude, longitude),
-//        icon:icon
-//    });
-//    PF('map').addOverlay(currentMarker);
+    document.getElementById('map-form:latitude').value = latitude;
+    document.getElementById('map-form:longitude').value = longitude;
     update();
 }
