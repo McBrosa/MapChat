64cff4f05acffebe5daddef4ffb073081c133d6c
csmcquay <csmcq_001@BITSEXC-008>
2016-04-26 00:00:58 -0400

    More documentation on files that needed.

--- src/java/com/mapchat/entitypackage/File1.java
+++ src/java/com/mapchat/entitypackage/File1.java
@@ -1,9 +1,12 @@
 /*
- * Created by Nathan Rosa on 2016.03.22  * 
- * Copyright © 2016 Nathan Rosa. All rights reserved. * 
+ * Created by MapChat Development Team
+ * Edited by Nathan Rosa, Corey McQuay
+ * Last Modified: 2016.03.28
+ * Copyright © 2016 MapChat Development Team. All rights reserved.
  */
 package com.mapchat.entitypackage;
 
+//Imports
 import com.mapchat.managers.Constants;
 import java.io.Serializable;
 import java.util.Date;
@@ -27,11 +30,15 @@ import javax.xml.bind.annotation.XmlRootElement;
 /**
  *
  * @author Nathan
+ * 
+ * 4/25/16
  */
+
+//Bean Properties
 @Entity
 @Table(name = "file")
 @XmlRootElement
-@NamedQueries({
+@NamedQueries({//Search Queries
     @NamedQuery(name = "File1.findAll", query = "SELECT f FROM File1 f"),
     @NamedQuery(name = "File1.findById", query = "SELECT f FROM File1 f WHERE f.id = :id"),
     @NamedQuery(name = "File1.findFilesByUserId", query = "SELECT f FROM File1 f WHERE f.userId.id = :userId"),
@@ -40,7 +47,11 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "File1.findByExtension", query = "SELECT f FROM File1 f WHERE f.extension = :extension"),
     @NamedQuery(name = "File1.findByTime", query = "SELECT f FROM File1 f WHERE f.time = :time"),
     @NamedQuery(name = "File1.findByGroupId", query = "SELECT f from File1 f WHERE f.groupId = :gid")})
+/**
+ * Class that represents the file in the database 
+ */
 public class File1 implements Serializable {
+    //Properties of the file in the database
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -69,14 +80,31 @@ public class File1 implements Serializable {
     @ManyToOne(optional = false)
     private User userId;
 
+    /**
+     * Default Constructor
+     */
     public File1() {
     }
 
+    /**
+     * Constructor that takes in the id
+     * 
+     * @param id id of the file
+     */
     public File1(Integer id) {
         this.id = id;
     }
     
+    /**
+     * Constructor of the File which takes in more parameters 
+     * @param id file's id in the database
+     * @param size size of the file 
+     * @param fileLocation where the file is located 
+     * @param extension //extension of the file 
+     * @param time /time where file is uploaded
+     */
     public File1(Integer id, int size, String fileLocation, String extension, Date time) {
+        //Sets globals to what gets passed in
         this.id = id;
         this.size = size;
         this.fileLocation = fileLocation;
@@ -85,74 +113,146 @@ public class File1 implements Serializable {
     }
     
         // This method is added to the generated code
-    public File1(String extension, User id) {
+    /**
+     * Constructor that takes in the extension and ID of the file
+     * @param extension files extension
+     * @param id file id
+     */
+    public File1(String extension, User id) 
+    {   //Sets the globals to get passed it
         this.extension = extension;
         userId = id;
     }
 
-    // This method is added to the generated code
+    /**
+     * Constructor that takes in the extension, used id, and group id
+     * @param extension extension of file
+     * @param uid user id 
+     * @param gid group id
+     */
     public File1(String extension, User uid, Groups gid) {
+        //Takes the globals and sets them to what gets passed in
         this.extension = extension;
         userId = uid;
         groupId = gid;
     }
     
+    /**
+     * Getter for the file's id
+     * @return file's current id
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Setter for the file's id
+     * @param id new file id
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Getter for the size global variable
+     * @return size of the file
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * Setter for the size method 
+     * @param size new size of the file
+     */
     public void setSize(int size) {
         this.size = size;
     }
-
+    
+    /**
+     * Getter for the file location
+     * @return the file location
+     */
     public String getFileLocation() {
         return fileLocation;
     }
 
+    /**
+     * Setter for the file location
+     * @param fileLocation the new file location
+     */
     public void setFileLocation(String fileLocation) {
         this.fileLocation = fileLocation;
     }
 
+    /**
+     * Gets the extension of the file
+     * @return the extension of the file
+     */
     public String getExtension() {
         return extension;
     }
 
+    /**
+     * Setter of the extension
+     * @param extension the new extension of the file
+     */
     public void setExtension(String extension) {
         this.extension = extension;
     }
 
+    /**
+     * Getter of the time of the file
+     * @return the time of the file
+     */
     public Date getTime() {
         return time;
     }
 
+    /**
+     * Setter of the time of the location
+     * @param time The time of the location
+     */
     public void setTime(Date time) {
         this.time = time;
     }
 
+    /**
+     * Gets the group id of the file
+     * @return Group Id of the file
+     */
     public Groups getGroupId() {
         return groupId;
     }
 
+    /**
+     * Sets the group id of the file
+     * @param groupId new group id of the file
+     */
     public void setGroupId(Groups groupId) {
         this.groupId = groupId;
     }
 
+    /**
+     * Gets the user id of the file
+     * @return the user id of the file
+     */
     public User getUserId() {
         return userId;
     }
 
+    /**
+     * Sets the new user id of the file
+     * @param userId userId of the file
+     */
     public void setUserId(User userId) {
         this.userId = userId;
     }
 
+    /**
+     * Represent the hashcode of the file object
+     * @return int of the hash of the file 
+     */
     @Override
     public int hashCode() {
         int hash = 0;
@@ -160,6 +260,11 @@ public class File1 implements Serializable {
         return hash;
     }
 
+    /**
+     * Override of the equals method of the file
+     * @param object the comparable object
+     * @return true if the same, false otherwise.
+     */
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
@@ -173,31 +278,59 @@ public class File1 implements Serializable {
         return true;
     }
 
+    /**
+     * Overriden implementation of the to string method of a file
+     * @return 
+     */
     @Override
     public String toString() {
         return "com.mapchat.entitypackage.File[ id=" + id + " ]";
     }
     
+    /**
+     * 
+     * @return 
+     */
     public String getFilePath() {
         return Constants.ROOT_DIRECTORY + getFilename();
     }
 
+    /**
+     * Sting representation of the file name
+     * @return the string of the file name
+     */
     public String getFilename() {
         return getId() + "." + getExtension();
     }
     
+    /**
+     * String representation of the thumbnail name
+     * @return The string representation of thumbnailname
+     */
     public String getThumbnailName() {
         return getId() + "_thumbnail." + getExtension();
     }    
     
+    /**
+     * String name of the icon
+     * @return  The string representation of the icon name
+     */
     public String getIconName() {
         return getId() + "_icon." + getExtension();
     }    
     
+    /**
+     * The thumbnail path string
+     * @return the thumbnail path in a string
+     */
     public String getThumbnailFilePath() {
         return Constants.ROOT_DIRECTORY + getThumbnailName();
     }
     
+    /**
+     * The icon file path in a string.
+     * @return String representation of the icon file path
+     */
     public String getIconFilePath() {
         return Constants.ROOT_DIRECTORY + getIconName();
     }
--- src/java/com/mapchat/jsfclassespackage/File1Controller.java
+++ src/java/com/mapchat/jsfclassespackage/File1Controller.java
@@ -1,10 +1,19 @@
+/*
+ * Created by MapChat Development Team
+ * Edited by Corey McQuay
+ * Last Modified: 2016.03.28
+ * Copyright © 2016 MapChat Development Team. All rights reserved.
+ */
+
 package com.mapchat.jsfclassespackage;
 
+//Imports project files 
 import com.mapchat.entitypackage.File1;
 import com.mapchat.jsfclassespackage.util.JsfUtil;
 import com.mapchat.jsfclassespackage.util.JsfUtil.PersistAction;
 import com.mapchat.sessionbeanpackage.File1Facade;
 
+//Import Java Files
 import java.io.Serializable;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -19,42 +28,73 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
+//Bean attributes
 @Named("fileController")
 @SessionScoped
 public class File1Controller implements Serializable {
 
+    //Global Variables 
     @EJB
     private com.mapchat.sessionbeanpackage.File1Facade ejbFacade;
     private List<File1> items = null;
     private File1 selected;
 
+    /**
+     *Constructor for the class to make the object
+     */
     public File1Controller() {
     }
 
+    /**
+     * Gets the selected file in the field
+     * @return the selected file.
+     */
     public File1 getSelected() {
         return selected;
     }
 
+    /**
+     * Setter of the selected file.
+     * @param selected the new selected file.
+     */
     public void setSelected(File1 selected) {
         this.selected = selected;
     }
 
+    /**
+     * Sets the Embeddable Keys so that keys can be accessed.
+     */
     protected void setEmbeddableKeys() {
     }
 
+    /**
+     *Initializes the embedded key
+     */
     protected void initializeEmbeddableKey() {
     }
 
+    /**
+     * Gets the Facade from the ejb
+     * @return The current ejbFacade
+     */
     private File1Facade getFacade() {
         return ejbFacade;
     }
 
+    /**
+     * Prepares and creates the selected file and initializes it into a embedabble key
+     * 
+     * @return
+     */
     public File1 prepareCreate() {
         selected = new File1();
         initializeEmbeddableKey();
         return selected;
     }
 
+    /**
+     * Creates the file into the DB
+     */
     public void create() {
         persist(PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("FileCreated"));
         if (!JsfUtil.isValidationFailed()) {
@@ -62,10 +102,16 @@ public class File1Controller implements Serializable {
         }
     }
 
+    /**
+     * Updates the file in the DB
+     */
     public void update() {
         persist(PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("FileUpdated"));
     }
 
+    /**
+     * Destroys the file in the DB
+     */
     public void destroy() {
         persist(PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("FileDeleted"));
         if (!JsfUtil.isValidationFailed()) {
@@ -74,17 +120,26 @@ public class File1Controller implements Serializable {
         }
     }
 
+    /**
+     * Gets The list of file items found
+     * @return The list of file items found
+     */
     public List<File1> getItems() {
-        if (items == null) {
+        if (items == null) { //Null Check
             items = getFacade().findAll();
         }
         return items;
     }
 
+    /**
+     * Calls seriazable persist function
+     * @param persistAction the action that needs to be done 
+     * @param successMessage message letting know if the db action was success or not
+     */
     private void persist(PersistAction persistAction, String successMessage) {
         if (selected != null) {
-            setEmbeddableKeys();
-            try {
+            setEmbeddableKeys(); //Sets the embedabble keys
+            try { //Delete action
                 if (persistAction != PersistAction.DELETE) {
                     getFacade().edit(selected);
                 } else {
@@ -97,7 +152,7 @@ public class File1Controller implements Serializable {
                 if (cause != null) {
                     msg = cause.getLocalizedMessage();
                 }
-                if (msg.length() > 0) {
+                if (msg.length() > 0) { //Error Messages
                     JsfUtil.addErrorMessage(msg);
                 } else {
                     JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
@@ -109,25 +164,42 @@ public class File1Controller implements Serializable {
         }
     }
 
+    /**
+     *
+     * @param id the file id number.
+     * @return The file in the database
+     * Finds the file in the db by the id
+     */
     public File1 getFile(java.lang.Integer id) {
         return getFacade().find(id);
     }
 
+    /**
+     * Selects many files based on relation
+     * @return all the files based off the facade.
+     */
     public List<File1> getItemsAvailableSelectMany() {
         return getFacade().findAll();
     }
 
+    /**
+     * Selects one of the files from available files
+     * @return
+     */
     public List<File1> getItemsAvailableSelectOne() {
         return getFacade().findAll();
     }
 
+    /**
+     * Converts the information into of the file controller
+     */
     @FacesConverter(forClass = File1.class)
     public static class FileControllerConverter implements Converter {
 
         @Override
         public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
             if (value == null || value.length() == 0) {
-                return null;
+                return null; //Null Check
             }
             File1Controller controller = (File1Controller) facesContext.getApplication().getELResolver().
                     getValue(facesContext.getELContext(), null, "fileController");
@@ -140,26 +212,36 @@ public class File1Controller implements Serializable {
             return key;
         }
 
+        /**
+         * Makes the key into the string 
+         * @param value The value to be turn into a string
+         * @return The key as a string
+         */
         String getStringKey(java.lang.Integer value) {
             StringBuilder sb = new StringBuilder();
             sb.append(value);
             return sb.toString();
         }
 
+        /**
+         * Overridable to Get the string of the File Controller Object
+         * @param facesContext the context of the face,
+         * @param component the component
+         * @param object the object
+         * @return
+         */
         @Override
         public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
             if (object == null) {
-                return null;
+                return null; //Null Check
             }
             if (object instanceof File1) {
-                File1 o = (File1) object;
-                return getStringKey(o.getId());
-            } else {
+                File1 o = (File1) object; //Gets the file
+                return getStringKey(o.getId()); //Prints out the object id
+            } else { //Logs the file controller string 
                 Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), File1.class.getName()});
-                return null;
+                return null; 
             }
         }
-
     }
-
 }
