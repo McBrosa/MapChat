4ca03de791e2f200d830da8bc49f87c88cde0d39
ADBarb <banth94@vt.edu>
2016-04-25 21:09:21 -0400

    chat renamed Chat and groups renamed Groups. chat.js moved into js folder. Some comments in XHTML.

--- src/java/com/mapchat/managers/Constants.java
+++ src/java/com/mapchat/managers/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     // Change /Users/Balci/FileStorageLocation/ below to /home/cs4984/Balci/FileStorageLocation/
     // for deployment to the server by replacing Balci with your last name.
     //-----------------------------------------------------------
-    public static final String ROOT_DIRECTORY = "C:/Users/Alan/FileStorageLocation/";
+    public static final String ROOT_DIRECTORY = "C:/Users/Tiger/Desktop/FileStorageLocation/";
 
     public static final String TEMP_FILE = "tmp_file";
     
